c392f573e73f1314a1ee7de6da331de497f1fed6
Melissa Alvares <143049151+melissa11e@users.noreply.github.com>
2024-06-09 03:34:45 -0300

    Add files via upload

---
+++ clinicahoje.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 27-Maio-2024 às 22:08
+-- Versão do servidor: 10.4.32-MariaDB
+-- versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `clinicahoje`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `admin`
+--
+
+CREATE TABLE `admin` (
+  `codigoAdmin` int(11) NOT NULL,
+  `emailAdmin` varchar(70) NOT NULL,
+  `senhaAdmin` char(16) NOT NULL CHECK (octet_length(`senhaAdmin`) >= 8)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `admin`
+--
+
+INSERT INTO `admin` (`codigoAdmin`, `emailAdmin`, `senhaAdmin`) VALUES
+(1, 'adm@gmail.com', '12345678'),
+(2, 'adm@gmail.com', '12345678');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `clog`
+--
+
+CREATE TABLE `clog` (
+  `codigo` int(11) NOT NULL,
+  `nome` varchar(70) DEFAULT NULL,
+  `cpf` char(11) DEFAULT NULL,
+  `email` varchar(70) DEFAULT NULL,
+  `telefone` varchar(12) DEFAULT NULL,
+  `senha` char(16) DEFAULT NULL CHECK (octet_length(`senha`) >= 8)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `clog`
+--
+
+INSERT INTO `clog` (`codigo`, `nome`, `cpf`, `email`, `telefone`, `senha`) VALUES
+(2, 'melissa', '11122233344', 'melissa@gmail.com', '11229900', 'teste123'),
+(7, 'melissa', '12345678910', 'mel@gmail', '12345678', 'abcdefgh');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`codigoAdmin`);
+
+--
+-- Índices para tabela `clog`
+--
+ALTER TABLE `clog`
+  ADD PRIMARY KEY (`codigo`),
+  ADD UNIQUE KEY `unique_email` (`email`),
+  ADD UNIQUE KEY `unique_cpf` (`cpf`),
+  ADD UNIQUE KEY `unique_telefone` (`telefone`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `codigoAdmin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `clog`
+--
+ALTER TABLE `clog`
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
